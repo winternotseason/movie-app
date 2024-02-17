460ae2730ae7c2ca0d1d09234750bfa256e7167b
winternotseason <winternotseason@naver.com>
2024-02-17 14:05:50 +0900

    feat : 순위 등락 여부

--- src/components/RankItem.js
+++ src/components/RankItem.js
@@ -3,6 +3,7 @@ import "./RankItem.scss";
 import poster from "../assets/dummy_poster.webp";
 import { FaWonSign } from "react-icons/fa";
 import { IoPersonSharp } from "react-icons/io5";
+import { IoCaretDownOutline, IoCaretUpOutline } from "react-icons/io5";
 
 const RankItem = ({ data }) => {
   const dataList = {
@@ -13,6 +14,7 @@ const RankItem = ({ data }) => {
     salesAcc: data.salesAcc,
     audiCnt: data.audiCnt,
     audiAcc: data.audiAcc,
+    rankOldAndNew: data.rankOldAndNew,
   };
   return (
     <div className="rankitem">
@@ -24,7 +26,26 @@ const RankItem = ({ data }) => {
           <div className="rank">
             <span className="number">{dataList.rank}</span>
             <span className="change">
-              {dataList.rankInten === "0" ? "-" : dataList.rankInten}
+              <p className="change-icon">
+                {dataList.rankOldAndNew === "NEW" ? (
+                  <p className="new">NEW</p>
+                ) : dataList.rankInten === "0" ? (
+                  <></>
+                ) : parseInt(dataList.rankInten) > 0 ? (
+                  <IoCaretUpOutline />
+                ) : (
+                  <IoCaretDownOutline />
+                )}
+              </p>
+              <p className="change-content">
+                {dataList.rankOldAndNew === "NEW" ? (
+                  <></>
+                ) : dataList.rankInten === "0" ? (
+                  "-"
+                ) : (
+                  Math.abs(parseInt(dataList.rankInten))
+                )}
+              </p>
             </span>
           </div>
           <div className="title">
@@ -38,7 +59,9 @@ const RankItem = ({ data }) => {
           </div>
           <span>
             <p>{parseInt(dataList.salesAmt).toLocaleString()}원</p>
-            <p className="acc">(누적 : {parseInt(dataList.salesAcc).toLocaleString()}원)</p>
+            <p className="acc">
+              (누적 : {parseInt(dataList.salesAcc).toLocaleString()}원)
+            </p>
           </span>
         </div>
         <div className="audience">
@@ -47,7 +70,9 @@ const RankItem = ({ data }) => {
           </div>
           <span>
             <p>{parseInt(dataList.audiCnt).toLocaleString()}명</p>
-            <p className="acc">(누적 : {parseInt(dataList.audiAcc).toLocaleString()}명)</p>
+            <p className="acc">
+              (누적 : {parseInt(dataList.audiAcc).toLocaleString()}명)
+            </p>
           </span>
         </div>
       </div>
